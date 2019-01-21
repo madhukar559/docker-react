865dd02021e36594881fce77224e2631f2ff731e
madhukar r <madhukar.rallabhandi@konzeptfabrica.com>
2019-01-21 20:24:46 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('renders without crashing dude', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders without crashing dude', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+
+it('renders without crashing dude', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
